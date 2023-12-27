97e0304d437c887d4101ead6aff4c57e2cb141e0
hoanmai06 <m.a.i.h.u.u.h.o.a.n@gmail.com>
2023-12-27 09:32:31 +0700

    Thêm khung nhân khẩu control

---
+++ src/main/control/NhanKhauControl.java
@@ -0,0 +1,25 @@
+package src.main.control;
+
+import src.main.boundary.nhankhau.NhanKhauModel;
+import src.main.entity.HoKhau;
+import src.main.entity.NhanKhau;
+
+import java.util.ArrayList;
+
+public class NhanKhauControl {
+    public ArrayList<NhanKhau> getList() {
+        return null;
+    }
+
+    public void add(NhanKhau entity) {
+
+    }
+
+    public void delete(NhanKhau entity) {
+
+    }
+
+    public void update(NhanKhau old_entity, NhanKhau new_entity) {
+
+    }
+}
