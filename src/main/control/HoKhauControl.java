04b17db3853352cb0220ebca2a67504473788842
nguyentuandung <nguyentunadung23012003@gmail.com>
2023-12-28 16:44:43 +0700

    Add method to get homeless people list in HoKhauControl

--- src/main/control/HoKhauControl.java
+++ src/main/control/HoKhauControl.java
@@ -9,8 +9,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class HoKhauControl {
     public static Statement statement_connect_to_sql_server() throws SQLException, ClassNotFoundException {
@@ -25,16 +27,24 @@ public class HoKhauControl {
         return null;
     }
 
-    public ArrayList<NhanKhau> getNhanKhauList() {
+    public ArrayList<NhanKhau> getNhanKhauVoGiaCuList() {
         return null;
     }
+
 
     public void add(HoKhau entity) {
         try{
             String sql = "insert into ho_khau values(";
-            sql = sql + entity.maHoKhau;
+            sql = sql + "'" + entity.maHoKhau + "'" + ", ";
+            sql = sql + "'" + entity.chuHo.CCCD + "'" + ", ";
+            sql = sql + "'" +  entity.khuVuc  + "'" + ", ";
+            sql = sql + "'" + entity.diaChi  + "'" + ", ";
+            String day = entity.ngayLap.substring(0,2);
+            String month = entity.ngayLap.substring(3,5);
+            String year = entity.ngayLap.substring(6,10);
+            sql = sql + "'" + year + "-" + month + "-" + day + "'" + ");";
+            System.out.println(sql);
             statement_connect_to_sql_server().execute(sql);
-
         }
         catch (Exception e){
             System.out.println(e.getMessage());
@@ -55,12 +65,12 @@ public class HoKhauControl {
         hoKhau1.maHoKhau = "1";
         hoKhau1.khuVuc = "HN001";
         hoKhau1.diaChi = "Dia chi 1";
-        hoKhau1.ngayLap = "12/08/1998";
+        hoKhau1.ngayLap = "27-12-2023";
 
-        hoKhau1.chuHo = new NhanKhau();
-        hoKhau1.chuHo.hoTen = "Nguyễn Văn A";
 
-         HoKhauControl control = new HoKhauControl();
-         control.add(hoKhau1);
+        hoKhau1.chuHo.CCCD = "1";
+
+        HoKhauControl control = new HoKhauControl();control.add(hoKhau1);
+
     }
 }
