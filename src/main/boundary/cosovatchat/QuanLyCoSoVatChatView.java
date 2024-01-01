ab6ba4b8ccacde894d60b0a9562b3aefa7a1ca6d
hoanmai06 <m.a.i.h.u.u.h.o.a.n@gmail.com>
2024-01-01 20:55:54 +0700

    Fix setView cơ sở vật chất

--- src/main/boundary/cosovatchat/QuanLyCoSoVatChatView.java
+++ src/main/boundary/cosovatchat/QuanLyCoSoVatChatView.java
@@ -26,12 +26,12 @@ public class QuanLyCoSoVatChatView extends FeatureView {
         setLayout(new GridLayout(0, 2));
         setBorder(new EmptyBorder(10, 10, 10, 10));
 
-        CoSoVatChatView coSoVatChatView = new CoSoVatChatView(new CoSoVatChatControlTestValue());
+        CoSoVatChatView coSoVatChatView = new CoSoVatChatView(coSoVatChatControl);
         coSoVatChatView.setBorder(new EmptyBorder(10, 10, 10, 10));
         add(coSoVatChatView);
         coSoVatChatControl.setView(coSoVatChatView);
 
-        PhongBanView phongBanView = new PhongBanView(new PhongBanControl());
+        PhongBanView phongBanView = new PhongBanView(phongBanControl);
         phongBanView.setBorder(new EmptyBorder(10, 10, 10, 10));
         add(phongBanView);
         phongBanControl.setPhongBanView(phongBanView);
