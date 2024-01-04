f7e4ee3f48ea4a29dda51e69a886658f1d1a7a99
hoanmai06 <m.a.i.h.u.u.h.o.a.n@gmail.com>
2024-01-05 05:17:51 +0700

    Sửa lại địa chỉ server sau khi push nhầm

--- src/main/control/ConnectionConfig.java
+++ src/main/control/ConnectionConfig.java
@@ -8,7 +8,7 @@ public class ConnectionConfig {
     public static Connection connect_to_sql_server() throws SQLException, ClassNotFoundException {
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 //        String url = "jdbc:sqlserver://LAPTOP-OBVO3M5Q\\CSDL:1433;databaseName=QUAN_LY_NHAN_KHAU;integratedSecurity=true;encrypt=true;trustServerCertificate=true";
-        String url = "jdbc:sqlserver://;serverName=192.168.1.5;port=1433;databaseName=QUAN_LY_NHAN_KHAU;authenticationScheme=NTLM;encrypt=true;trustServerCertificate=true";
+        String url = "jdbc:sqlserver://;serverName=::1;port=1433;databaseName=QUAN_LY_NHAN_KHAU;integratedSecurity=true;encrypt=true;trustServerCertificate=true";
         String username = "nmcnpm_user";
         String password = "nmcnpm2023";
         Connection connection = DriverManager.getConnection(url, username, password);
